8b1e0ed79e468a1aef463892fe8e7e884500ed9c
brandonmongo <brandonmongo@hotmail.com>
2021-02-23 02:12:45 +0000

    added search product function

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,9 +9,22 @@ from .models import Product
 def true_products(request):
     """ A view to display all the products """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'true-search' in request.GET:
+            query = request.GET['true-search']
+            if not query:
+                messages.error(request, 'You didnt search anything')
+                return redirect(reverse('products'))
+
+            queries = (
+                        Q(Name__icontains=query) | Q(product_description__icontains=query) | Q(Brand__icontains=query))
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_product': query
     }
 
     return render(request, 'products/true_products.html', context)
