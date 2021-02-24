88989e869fba6ecc9895bd14e8a3f5ac25864ac3
brandonmongo <brandonmongo@hotmail.com>
2021-02-24 11:00:33 +0000

    Added sorting and product count to product page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -11,8 +12,25 @@ def true_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(Category__name__in=categories)
@@ -28,10 +46,13 @@ def true_products(request):
                         Q(Name__icontains=query) | Q(product_description__icontains=query) | Q(Brand__icontains=query))
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_product': query,
-        'current_categories': categories
+        'current_categories': categories,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'products/true_products.html', context)
