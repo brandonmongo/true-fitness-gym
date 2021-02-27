5a4335bf6e508a7c40ba217d1082fffed6b971df
brandonmongo <brandonmongo@hotmail.com>
2021-02-27 21:02:03 +0000

    added weight selector to product detail and added weight logic

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,15 +11,27 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
 
-    for product_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.Price
-        product_count += quantity
-        cart_items.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product
-        })
+    for product_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=product_id)
+            total += item_data * product.Price
+            product_count += item_data
+            cart_items.append({
+                'product_id': product_id,
+                'quantity': item_data,
+                'product': product
+            })
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            for weight, quantity in item_data['product_by_weight'].items():
+                total += quantity * product.Price
+                product_count += quantity
+                cart_items.append({
+                    'product_id': product_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'weight': weight
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.DELIVERY_PERCENTAGE / 100)
--- cart/views.py
+++ cart/views.py
@@ -14,12 +14,24 @@ def add_to_cart(request, product_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    weight = None
+    if 'product_weight' in request.POST:
+        weight = request.POST['product_weight']
     cart = request.session.get('cart', {})
 
-    if product_id in list(cart.keys()):
-        cart[product_id] += quantity
+    if weight:
+        if product_id in list(cart.keys()):
+            if weight in cart[product_id]['product_by_weight'].keys():
+                cart[product_id]['product_by_weight'][weight] += quantity
+            else:
+                cart[product_id]['product_by_weight'][weight] = quantity
+        else:
+            cart[product_id] = {'product_by_weight': {weight: quantity}}
     else:
-        cart[product_id] = quantity
+        if product_id in list(cart.keys()):
+            cart[product_id] += quantity
+        else:
+            cart[product_id] = quantity
 
     request.session['cart'] = cart
     print(request.session['cart'])
