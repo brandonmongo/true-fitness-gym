91355abdda152a5114871c06efc057f1b2a01cde
brandonmongo <brandonmongo@hotmail.com>
2021-03-01 17:05:10 +0000

    added adjust cart and remove cart view and calc subtotal template filter

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -37,3 +37,50 @@ def add_to_cart(request, product_id):
     print(request.session['cart'])
 
     return redirect(redirect_url)
+
+
+def adjust_cart(request, product_id):
+    """ A view that adjust the quantity of a specific product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    weight = None
+    if 'product_weight' in request.POST:
+        weight = request.POST['product_weight']
+    cart = request.session.get('cart', {})
+
+    if weight:
+        if quantity > 0:
+            cart[product_id]['product_by_weight'][weight] = quantity
+            if not cart[product_id]['product_by_weight']:
+                cart.pop(product_id)
+        else:
+            del cart[product_id]['product_by_weight'][weight]
+    else:
+        cart.pop(product_id)
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(reverse('cart'))
+
+
+def remove_from_cart(request, product_id):
+    """ A view to remove item from the cart """
+    try:
+        weight = None
+        if 'product_weight' in request.POST:
+            weight = request.POST['product_weight']
+        cart = request.session.get('cart', {})
+
+        if weight:
+            del cart[product_id]['product_by_weight'][weight]
+            if not cart[product_id]['product_by_weight']:
+                cart.pop(product_id)
+        else:
+            cart.pop(product_id)
+
+        request.session['cart'] = cart
+        print(request.session['cart'])
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
