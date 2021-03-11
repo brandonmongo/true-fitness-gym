a7a70014d516b9a66d67a1f35f7dc73a7f1a5331
brandonmongo <brandonmongo@hotmail.com>
2021-03-11 12:06:44 +0000

    added webhook handler comments

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 
 
 class stripeWH_Handler:
-    """handle stripe webhooks""""
+    """handle stripe webhooks"""
     def __init__(self, request):
         self.request = request
 
     def handle_event(self, request):
+        """handle any webhook event"""
         return HttpResponse(
             content=f'webhook recieved: {event["type"]}',
             status=200)
