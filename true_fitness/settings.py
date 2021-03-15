a989e9e74cf0427e71fe43d16c05fad680d279a6
brandonmongo <brandonmongo@hotmail.com>
2021-03-15 17:49:36 +0000

    updated secret key and debug

--- true_fitness/settings.py
+++ true_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'keb^jb&v^p&c7_ky==&*crpsl#v2^*ul9_9hp(zsqderkz(as_'
+SECRET_KEY = os.environ.get('TRUE_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['brandon-true-fitness.herokuapp.com', 'localhost']
 
