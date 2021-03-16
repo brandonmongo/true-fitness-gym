a47dfa4a30f6c745253530ac370482f8f6ee40b4
brandonmongo <brandonmongo@hotmail.com>
2021-03-16 16:06:56 +0000

    added cache control

--- true_fitness/settings.py
+++ true_fitness/settings.py
@@ -177,6 +177,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Wed, 31 Dec 3000 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # bucket Config
     AWS_STORAGE_BUCKET_NAME = 'brandon-true-fitness'
     AWS_S3_REGION_NAME = 'eu-west-2'
