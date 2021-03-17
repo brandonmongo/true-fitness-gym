3c1c0856c5488f777b2a40461e502c0d11005108
brandonmongo <brandonmongo@hotmail.com>
2021-03-17 16:13:04 +0000

    added blog view and url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Post
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template = 'blog/blog.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template = 'blog/post_detail.html'
