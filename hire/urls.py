a9fa6195909ca37fe53c4459f02a9023349f7fce
brandonmongo <brandonmongo@hotmail.com>
2021-03-21 14:19:04 +0000

    added PT details view and template

--- hire/urls.py
+++ hire/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.all_PTs, name='all_PTs')
+    path('', views.all_PTs, name='all_PTs'),
+    path('<PT_slug>/', views.PTs_details, name='PTs_details')
 ]
--- hire/views.py
+++ hire/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import PT
 # Create your views here.
 
@@ -6,10 +6,22 @@ from .models import PT
 def all_PTs(request):
     """ A view to display all the PTs at the gym """
 
-    PTs = PT.objects.all()
+    PTs = PT.objects.all().order_by("full_name")
     template = 'hire/hire_PT.html'
     context = {
         'PTs': PTs
     }
 
+    return render(request, template, context)
+
+
+def PTs_details(request, PT_slug):
+    """ A view to display all the PTs information """
+    personal_trainer = get_object_or_404(PT, slug=PT_slug)
+
+    print(personal_trainer)
+    template = 'hire/PT_detail.html'
+    context = {
+        'personal_trainer': personal_trainer
+    }
     return render(request, template, context)
