fe6fcddabf43b3e0d3e9019d48a0221aeb932bb5
brandonmongo <brandonmongo@hotmail.com>
2021-03-21 01:58:26 +0000

    added hire view and template

--- hire/views.py
+++ hire/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import PT
 # Create your views here.
+
+
+def all_PTs(request):
+    """ A view to display all the PTs at the gym """
+
+    PTs = PT.objects.all()
+    template = 'hire/hire_PT.html'
+    context = {
+        'PTs': PTs
+    }
+
+    return render(request, template, context)
